4aa176fd9622d1747da61d430b8e377014ae138a
zsquaredz <zhaozheng1997@hotmail.com>
2020-02-07 02:30:38 +0000

    added func to anonymize

--- scripts/xsum-preprocessing-convs2s.py
+++ scripts/xsum-preprocessing-convs2s.py
@@ -1,6 +1,10 @@
 import sys
 import os
 import json
+import spacy
+import random
+
+random.seed(2020)
 
 dm_single_close_quote = u'\u2019' # unicode
 dm_double_close_quote = u'\u201d'
@@ -47,6 +51,30 @@ def get_data_from_file(story_file):
 
   return article
 
+def get_article_summary_from_file_and_anonymize(articlefile, summaryfile):
+    entityDict = {}
+    entitySet = set()
+    article_lines = read_text_file(articlefile)
+    summary_lines = read_text_file(summaryfile)
+    article_lines = [fix_missing_period(line) for line in article_lines]
+    summary_lines = [fix_missing_period(line) for line in summary_lines]
+    article = ' '.join(article_lines)
+    summary = ' '.join(summary_lines)
+    # obtain the ent text using spaCy model
+    doc = nlp(article+summary)
+    for ent in doc.ents:
+        entitySet.add(ent.text)
+    entityId = [i for i in range(len(entitySet))]
+    random.shuffle(entityId)
+    for i, entity in enumerate(entitySet):
+        entitytag = '@entity'+str(entityId[i])
+        entityDict[entitytag] = entity
+        # replace this entity in article/summary with entity tag
+        article = ' '.join([entitytag if token == entity else token for token in article.split()])
+        summary = ' '.join([entitytag if token == entity else token for token in summary.split()])
+
+    return article.lower(), summary.lower(), entityDict
+
 def write_to_bin(data_type, out_file_rb, out_file_fs):
   
   """Reads all the bbids and write them to out file."""
@@ -81,13 +109,53 @@ def write_to_bin(data_type, out_file_rb, out_file_fs):
     
   print("Finished writing file %s, %s\n" %(out_file_rb, out_file_fs))
 
+def write_to_bin_anonymized(data_type, out_file_rb, out_file_fs):
+  """Reads all the bbids and write them to out file."""
+  print("Making text file for bibids listed as %s..." % data_type)
+
+  bbcids = train_dev_test_dict[data_type]
+  num_stories = len(bbcids)
+
+  rb_foutput = open(out_file_rb, "w")
+  fs_foutput = open(out_file_fs, "w")
+
+  for idx, s in enumerate(bbcids):
+
+    if idx % 1000 == 0:
+      print("Writing story %i of %i; %.2f percent done" % (idx, num_stories, float(idx) * 100.0 / float(num_stories)))
+
+    # Files
+    restbodyfile = bbc_tokenized_stories_dir + "/restbody/" + s + ".restbody"
+    firstsentencefile = bbc_tokenized_stories_dir + "/firstsentence/" + s + ".fs"
+    entityfile = finished_files_dir + "/entitymapping/" + s + ".ent"
+
+    # Get the strings to write to .bin file
+    # abstract = get_data_from_file(firstsentencefile)
+    # article = get_data_from_file(restbodyfile)
+    # article = " ".join(article.strip().split()[:400])
+    article, abstract, entityDict = get_article_summary_from_file_and_anonymize(summaryfile=firstsentencefile, articlefile=restbodyfile)
+
+    with open(entityfile, 'w') as file:
+        file.write(json.dumps(entityDict))  # use `json.loads` to do the reverse
+
+    rb_foutput.write(article + "\n")
+    fs_foutput.write(abstract + "\n")
+
+  rb_foutput.close()
+  fs_foutput.close()
+
+  print("Finished writing file %s, %s\n" % (out_file_rb, out_file_fs))
+
 if __name__ == '__main__':
 
   # Create some new directories
   if not os.path.exists(finished_files_dir): os.makedirs(finished_files_dir)
 
+  nlp = spacy.load("en_core_web_sm")
+  print("spaCy model loaded")
+
   # Read the tokenized stories, do a little postprocessing then write to text files
-  write_to_bin("test", os.path.join(finished_files_dir, "test.document"), os.path.join(finished_files_dir, "test.summary"))
-  write_to_bin("validation", os.path.join(finished_files_dir, "validation.document"), os.path.join(finished_files_dir, "validation.summary"))
-  write_to_bin("train", os.path.join(finished_files_dir, "train.document"), os.path.join(finished_files_dir, "train.summary"))
+  write_to_bin_anonymized("test", os.path.join(finished_files_dir, "test.anonymized.document"), os.path.join(finished_files_dir, "test.anonymized.summary"))
+  # write_to_bin_anonymized("validation", os.path.join(finished_files_dir, "validation.anonymized.document"), os.path.join(finished_files_dir, "validation.anonymized.summary"))
+  # write_to_bin_anonymized("train", os.path.join(finished_files_dir, "train.anonymized.document"), os.path.join(finished_files_dir, "train.anonymized.summary"))
 	
